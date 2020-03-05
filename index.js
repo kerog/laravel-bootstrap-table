a7645259499da129db69e970e91129ccb693a744
kerog (st) <konrad@kerogos.pl>
2020-03-05 22:46:12 +0100

    intitialization of repo
    Signed-off-by: kerog (st) <konrad@kerogos.pl>

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * @author: KEROGOS
+ * @version: v0.0.1
+ */
+
+(function ($) {
+	'use strict';
+
+	$.extend($.fn.bootstrapTable.defaults, {
+		orginalUrl: '',
+		query: '',
+	});
+
+	var BootstrapTable = $.fn.bootstrapTable.Constructor,
+		_updatePagination = BootstrapTable.prototype.updatePagination,
+		_initServer = BootstrapTable.prototype.initServer;
+
+	BootstrapTable.prototype.updatePagination = function(){
+		if(this.options.sidePagination === 'server'){
+			this.options.url = this.options.orginalUrl + (this.options.pageNumber ? '?page='+ this.options.pageNumber :'');
+		}
+		_updatePagination.apply(this, Array.prototype.slice.apply(arguments));
+	};
+
+	BootstrapTable.prototype.initServer = function (silent, query) {
+		if(this.options.sidePagination === 'server'){
+			if(query === null || query === undefined){
+				query= this.options.query;
+			} else
+				this.options.query = query;
+//			_initServer.apply(this, Array.prototype.slice.apply(arguments));
+			_initServer.apply(this, [silent, query]);
+		}
+	};
+
+})(jQuery);
